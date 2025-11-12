ff4697e094a5700b38e72a3d52aa4f6830b1e17b
Diego Arturo Barriguete <dbarriguete@gmail.com>
2025-11-12 08:16:57 -0600

    Actualización de README.md y creación de proyecto inicial de consola

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+string? opcionElegida = string.Empty;
+
+while (true)
+{
+    Console.WriteLine("Bienvenido a los ejemplos de Ideas Exponenciales.");
+    Console.WriteLine("A continuación elige la opción que deseas utilizar:\n\n");
+    Console.WriteLine("1) Comparación con double");
+    Console.WriteLine("2) Comparación con decimal");
+    Console.WriteLine("\nX) Salir\n\n");
+    Console.Write("escribe tu elección y presiona <enter>: ");
+    opcionElegida = (Console.ReadLine()??string.Empty).ToUpper();
+
+    switch (opcionElegida)
+    {
+        case "1":
+            break;
+        case "2":
+            break;
+    }
+    if(opcionElegida.Equals("X"))
+    {
+        break;
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # EjemplosCodigo2025
 Repositorio con ejemplos de código escritos en C#. (desde la versión 8)
+
+Este repositorio cuenta con una aplicación de consola en .net, en donde la misma aplicación de consola permitirá al usuario elegir que tareas ejecutar.
+
+El código muestra ejemplos básicos del uso de C#.
+
+## Ejemplos incluidos:
+- Comparación inexacta de valores de variables tipo Double.
+- Comparación valores de variables tipo Decimal.
+
+
+Desarrollado por: Diegue B.
+
